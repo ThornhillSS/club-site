e681095c1c623dcd745edb1c24b8812cd20afcb4
Terrarian <72149678+Terra-rian@users.noreply.github.com>
2022-10-07 11:58:20 -0400

    dark mode fix part 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
                     </a>
                 </div>
                 <div className="flex items-center mt-6">
-                    <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
+                    <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900">
                         &copy; {new Date().getFullYear()} designed by{' '}
                         <a href="https://github.com/Terra-rian" target='_blank' rel="noreferrer">
                             Timothy I
