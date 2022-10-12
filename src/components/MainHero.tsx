2711d24008322efa109cab94de9a48f53bf071d1
Adam Chen <encodeous7@gmail.com>
2022-10-11 22:26:14 -0400

    fixie

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -14,7 +14,7 @@ const MainHero = () => {
             <div className={'sm:text-center lg:text-left'}>
                 <h1 className={'text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'}>
                     <NonSSR>
-                        <span className={'text-4xl block xl:inline'}>
+                        <span className={'text-4xl block'}>
                             {config.mainHero.titles[Math.floor(Math.random() * config.mainHero.titles.length)]}
                         </span>{' '}
                     </NonSSR>
